abdd3b685ad45ec970277f0409b6e0e1a9cf843c
mugil1515 <mugil1504@gmail.com>
2025-07-13 08:46:25 +0530

    Create country.java

---
+++ country.java
@@ -0,0 +1,37 @@
+package com.example;
+
+public class Country {
+    private String name;
+    private String capital;
+    private int population;
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    @Override
+    public String toString() {
+        return "Country{name='" + name + "', capital='" + capital + "', population=" + population + '}';
+    }
+}
